4b335298b1bc51af2a509b1987cd9e39875a19b5
Juan <90869650+Juanfrxz@users.noreply.github.com>
2025-05-26 16:41:20 -0500

    feat: :sparkles: integrate driver.js for guided tour functionality in tour.js and include script in index.html

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -1,117 +1,118 @@
 import { driver } from "driver.js";
 import "driver.js/dist/driver.css";
 
-const driverObj = driver({
-  showProgress: true,
-  steps: [
-    {
-      element: ".about__container",
-      popover: {
-        title: "Hola Soy Juan",
-        description: "Soy desarrollador de software.",
+document.addEventListener('DOMContentLoaded', () => {
+  const driverObj = driver({
+    showProgress: true,
+    steps: [
+      {
+        element: ".about__container",
+        popover: {
+          title: "Hola Soy Juan",
+          description: "Soy desarrollador de software.",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: ".home__img",
-      popover: {
-        title: "Este es mi Modelo 3D",
-        description:
-          "Este modelo es una representacion de mi persona, está hecho con Three.js.",
+      {
+        element: ".home__img",
+        popover: {
+          title: "Este es mi Modelo 3D",
+          description:
+            "Este modelo es una representacion de mi persona, está hecho con Three.js.",
+        },
+        position: "left",
       },
-      position: "left",
-    },
-    {
-      element: '[data-section="home"][data-value="contact"]',
-      popover: {
-        title: "Aqui puedes contactarme",
-        description: "Haz clic aquí para contactarme vía WhatsApp.",
+      {
+        element: '[data-section="home"][data-value="contact"]',
+        popover: {
+          title: "Aqui puedes contactarme",
+          description: "Haz clic aquí para contactarme vía WhatsApp.",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: '[data-section="home"][data-value="download"]',
-      popover: {
-        title: "Esta es mi hoja de vida",
-        description: "Descarga mi CV en inglés y español.",
+      {
+        element: '[data-section="home"][data-value="download"]',
+        popover: {
+          title: "Esta es mi hoja de vida",
+          description: "Descarga mi CV en inglés y español.",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: "#linkedin",
-      popover: {
-        title: "Linkedin",
-        description: "Aqui podras encontrar mi perfil de linkedin",
+      {
+        element: "#linkedin",
+        popover: {
+          title: "Linkedin",
+          description: "Aqui podras encontrar mi perfil de linkedin",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: "#instagram",
-      popover: {
-        title: "Instagram",
-        description: "Aqui podras encontrar mi perfil de instagram",
+      {
+        element: "#instagram",
+        popover: {
+          title: "Instagram",
+          description: "Aqui podras encontrar mi perfil de instagram",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: "#github",
-      popover: {
-        title: "Github",
-        description: "Aqui podras encontrar mi perfil de github",
+      {
+        element: "#github",
+        popover: {
+          title: "Github",
+          description: "Aqui podras encontrar mi perfil de github",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: "[data-section='skills'][data-value='title']",
-      popover: {
-        title: "Habilidades",
-        description: "Aquí tienes algunas de mis habilidades técnicas.",
+      {
+        element: "[data-section='skills'][data-value='title']",
+        popover: {
+          title: "Habilidades",
+          description: "Aquí tienes algunas de mis habilidades técnicas.",
+        },
+        position: "bottom",
       },
-      position: "bottom",
-    },
-    {
-      element: "#frontend",
-      popover: {
-        title: "frontend",
-        description:
-          "Aquí tienes algunas de mis habilidades técnicas en frontend",
+      {
+        element: "#frontend",
+        popover: {
+          title: "frontend",
+          description:
+            "Aquí tienes algunas de mis habilidades técnicas en frontend",
+        },
+        position: "top",
       },
-      position: "top",
-    },
-    {
-      element: "#backend",
-      popover: {
-        title: "backend",
-        description:
-          "Aquí tienes algunas de mis habilidades técnicas en backend",
+      {
+        element: "#backend",
+        popover: {
+          title: "backend",
+          description:
+            "Aquí tienes algunas de mis habilidades técnicas en backend",
+        },
+        position: "top",
       },
-      position: "top",
-    },
-    {
-      element: "#tools",
-      popover: {
-        title: "tools and others",
-        description: "Aquí tienes algunas de mis habilidades técnicas en tools",
+      {
+        element: "#tools",
+        popover: {
+          title: "tools and others",
+          description: "Aquí tienes algunas de mis habilidades técnicas en tools",
+        },
+        position: "top",
       },
-      position: "top",
-    },
-    {
-      element: "[data-section='projects'][data-value='title']",
-      popover: {
-        title: "Proyectos",
-        description: "Estos son algunos de mis proyectos destacados.",
+      {
+        element: "[data-section='projects'][data-value='title']",
+        popover: {
+          title: "Proyectos",
+          description: "Estos son algunos de mis proyectos destacados.",
+        },
+        position: "top",
       },
-      position: "top",
-    },
-    {
-      element: "#f1_title",
-      popover: {
-        title: "F1 - Formula1",
-        description: "Este es mi proyecto de Formula 1, unos de mis mejores proyectos.",
+      {
+        element: "#f1_title",
+        popover: {
+          title: "F1 - Formula1",
+          description: "Este es mi proyecto de Formula 1, unos de mis mejores proyectos.",
+        },
+        position: "top",
       },
-      position: "top",
-    },
-    {
+      {
         element: "#f1_content",
         popover: {
           title: "F1 - Formula1",
@@ -127,14 +128,14 @@ const driverObj = driver({
         },
         position: "top",
       },
-
-  ],
-});
-
-const startTourBtn = document.getElementById('start-tour');
-if (startTourBtn) {
-  startTourBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    driverObj.drive();
+    ],
   });
-}
+
+  const startTourBtn = document.getElementById('start-tour');
+  if (startTourBtn) {
+    startTourBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      driverObj.drive();
+    });
+  }
+});
